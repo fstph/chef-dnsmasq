0d2b767c74f2f85baaf450bf86f75f3e9f66be9f
Adrian Moisey <adrian@yola.com>
2013-07-29 12:32:55 +0200

    Add missing metadata.rb
    As per https://github.com/fstph/chef-dnsmasq/pull/2

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Jörg Herzinger, Fachschaft Technische Physik"
+maintainer_email "joerg.herzinger@oiml.at"
+license          "Apache 2.0"
+description      "Installs/Configures dnsmasq"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
